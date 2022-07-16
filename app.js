63aff62d8a81714313fc6822f7a913427d6abd5a
sod-diq <mmsardiqque@gmail.com>
2022-07-16 10:59:55 -0700

    enter

---
+++ app.js
@@ -0,0 +1,13 @@
+let btns = document.querySelectorAll('.btn-container button')
+let card2 = document.querySelector('.card-2')
+let card = document.querySelector('.card')
+let span = document.querySelector('span')
+
+Array.from(btns).forEach(btn => {
+  btn.addEventListener('click' , e => {
+    const rating = e.target.textContent
+    span.textContent = rating
+    card2.classList.remove('hide')
+    card.classList.add('hide')
+  })
+})
